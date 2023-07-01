70bbef76888c6a365c2e470bc07ea12d4c78a2cb
nainglinnkhant <nainglinnkhnt@gmail.com>
2023-07-02 03:17:25 +0630

    feat: implement vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from '@vercel/analytics/react'
+
 import { Providers } from './providers'
 import './globals.css'
 
@@ -27,6 +29,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang='en' suppressHydrationWarning>
       <body>
         <Providers>{children}</Providers>
+
+        <Analytics />
       </body>
     </html>
   )
